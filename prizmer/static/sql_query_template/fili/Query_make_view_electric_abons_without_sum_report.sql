67a33e8630fa6af395422fadecac02f82bbb8502
Lena <semercidis@bk.ru>
2017-06-02 14:02:10 +0300

    Фили, отчёты в разделе экономика

--- prizmer/static/sql_query_template/fili/Query_make_view_electric_abons_without_sum_report.sql
+++ prizmer/static/sql_query_template/fili/Query_make_view_electric_abons_without_sum_report.sql
@@ -1,4 +1,9 @@
-﻿ SELECT electric_abons_report.account_2, electric_abons_report.date_install, 
+﻿-- View: electric_abons_without_sum_report
+
+-- DROP VIEW electric_abons_without_sum_report;
+
+CREATE OR REPLACE VIEW electric_abons_without_sum_report AS 
+ SELECT electric_abons_report.account_2, electric_abons_report.date_install, 
     electric_abons_report.name_meter, electric_abons_report.type_energo, 
     electric_abons_report.ab_name, electric_abons_report.obj_name, 
     electric_abons_report.name_params, 
@@ -7,3 +12,7 @@
    FROM electric_abons_report
   WHERE electric_abons_report.type_energo <> 'Электричество'::text
   ORDER BY electric_abons_report.account_2, electric_abons_report.obj_name, electric_abons_report.ab_name;
+
+ALTER TABLE electric_abons_without_sum_report
+  OWNER TO postgres;
+
