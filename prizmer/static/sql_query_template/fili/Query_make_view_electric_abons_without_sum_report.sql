280805595c3b58e52a948dcf12e4eee2618266c8
Lena <semercidis@bk.ru>
2017-06-08 12:19:08 +0300

    Обновлён запрос для формирования отчёта по филям, по эл-ву Query_make_view_electric_abons_without_sum_report.sql На головинке и филях обновила

--- prizmer/static/sql_query_template/fili/Query_make_view_electric_abons_without_sum_report.sql
+++ prizmer/static/sql_query_template/fili/Query_make_view_electric_abons_without_sum_report.sql
@@ -8,8 +8,9 @@ CREATE OR REPLACE VIEW electric_abons_without_sum_report AS
     electric_abons_report.ab_name, electric_abons_report.obj_name, 
     electric_abons_report.name_params, 
     electric_abons_report.factory_number_manual, 
-    (electric_abons_report.name_meter::text || '-'::text) || "substring"(electric_abons_report.type_energo, char_length(electric_abons_report.type_energo), char_length(electric_abons_report.type_energo)) AS report_factory_number_manual,
-    (electric_abons_report.factory_number_manual||'-'||"substring"(electric_abons_report.type_energo, char_length(electric_abons_report.type_energo), char_length(electric_abons_report.type_energo))) as report_num_meter
+    electric_abons_report.name_meter::text AS report_factory_number_manual, 
+    substring(electric_abons_report.name_params, 0,3)||'-'||electric_abons_report.factory_number_manual AS report_num_meter
+   
    FROM electric_abons_report
   WHERE electric_abons_report.type_energo <> 'Электричество'::text
   ORDER BY electric_abons_report.account_2, electric_abons_report.obj_name, electric_abons_report.ab_name;
