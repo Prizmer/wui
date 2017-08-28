3b6a6cdad4de29483e45d2478e9add2ca5848d6d
Lena <semercidis@bk.ru>
2017-08-28 16:57:48 +0300

    Подсветка Н/Д в шаблонах

--- prizmer/static/sql_query_template/Query_make_water_pulsar_abons.sql
+++ prizmer/static/sql_query_template/Query_make_water_pulsar_abons.sql
@@ -1,25 +1,15 @@
-﻿CREATE VIEW water_pulsar_abons as
-(
-SELECT 
-  objects.guid as obj_guid, 
-  objects.name as obj_name, 
-  abonents.guid as ab_guid, 
-  abonents.name as ab_name, 
-  meters.name as meter_name, 
-  meters.factory_number_manual, 
-  types_meters.name
-FROM 
-  public.abonents, 
-  public.objects, 
-  public.link_abonents_taken_params, 
-  public.taken_params, 
-  public.meters, 
-  public.types_meters
-WHERE 
-  abonents.guid_objects = objects.guid AND
-  link_abonents_taken_params.guid_abonents = abonents.guid AND
-  link_abonents_taken_params.guid_taken_params = taken_params.guid AND
-  taken_params.guid_meters = meters.guid AND
-  meters.guid_types_meters = types_meters.guid and
-  (types_meters.name='Пульсар ГВС' or types_meters.name='Пульсар ХВС')
-  )
+﻿-- View: water_pulsar_abons
+
+-- DROP VIEW water_pulsar_abons;
+
+CREATE OR REPLACE VIEW water_pulsar_abons AS 
+ SELECT objects.guid AS obj_guid, objects.name AS obj_name, 
+    abonents.guid AS ab_guid, abonents.name AS ab_name, 
+    meters.name AS meter_name, meters.factory_number_manual, types_meters.name
+   FROM abonents, objects, link_abonents_taken_params, taken_params, meters, 
+    types_meters
+  WHERE abonents.guid_objects::text = objects.guid::text AND link_abonents_taken_params.guid_abonents::text = abonents.guid::text AND link_abonents_taken_params.guid_taken_params::text = taken_params.guid::text AND taken_params.guid_meters::text = meters.guid::text AND meters.guid_types_meters::text = types_meters.guid::text AND (types_meters.name::text = 'Пульсар ГВС'::text OR types_meters.name::text = 'Пульсар ХВС'::text);
+
+ALTER TABLE water_pulsar_abons
+  OWNER TO postgres;
+
